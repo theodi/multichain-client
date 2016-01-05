730fea16831eea439a72accd805d57e8058d5a1e
pikesley <github@orgraphone.org>
2016-01-05 11:54:08 +0000

    No idea

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -1,8 +1,9 @@
 require 'vcr'
 require 'webmock/cucumber'
+require 'aruba/cucumber'
 
 VCR.configure do |c|
-  c.default_cassette_options = { :record => :once }
+  c.default_cassette_options = { record: :once }
   c.cassette_library_dir = 'features/support/vcr'
   c.hook_into :webmock
   c.allow_http_connections_when_no_cassette = false
@@ -11,6 +12,7 @@ end
 VCR.cucumber_tags do |t|
   t.tag '@vcr', use_scenario_name: true
 end
+
 class VcrFriendlyMain
   def initialize argv, stdin, stdout, stderr, kernel
     @argv, @stdin, @stdout, @stderr, @kernel = argv, stdin, stdout, stderr, kernel
@@ -22,4 +24,20 @@ class VcrFriendlyMain
     $stderr = @stderr
     Multichain::CLI.start @argv
   end
+end
+
+Before '@vcr' do
+  Aruba.configure do |config|
+    config.command_launcher = :in_process
+    config.main_class = VcrFriendlyMain
+  end
+end
+
+After '@vcr' do
+  Aruba.configure { |config| config.command_launcher = :spawn }
+
+  VCR.eject_cassette
+  $stdin = STDIN
+  $stdout = STDOUT
+  $stderr = STDERR
 end
