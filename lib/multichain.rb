609d43182f9c2e21985b388cc6886ff415ff143f
pezholio <pezholio@gmail.com>
2016-01-05 15:22:09 +0000

    Add simple calls to client

--- lib/multichain.rb
+++ lib/multichain.rb
@@ -2,5 +2,6 @@ require 'httparty'
 require 'thor'
 
 require 'multichain/cli'
+require 'multichain/client'
 require 'multichain/encoder'
 require 'multichain/version'
---
+++ lib/multichain/client.rb
@@ -0,0 +1,41 @@
+module Multichain
+  class Client
+
+    def initialize(username, password, host, port = 2898)
+      @username = username
+      @password = password
+      @host = host
+      @port = port
+    end
+
+    def auth
+      "#{@username}:#{@password}"
+    end
+
+    def url
+      "http://#{auth}@#{@host}:#{@port}"
+    end
+
+    def method_missing(sym, params = [])
+      request = build_request(sym.to_s, params)
+      send_request(request)
+    end
+
+    private
+
+      def build_request(method, params)
+        {
+          method: method,
+          params: params,
+          id: 'whevs'
+        }.to_json
+      end
+
+      def send_request(request)
+        post = HTTParty.post(url, body: request, headers: {"Content-Type" => "application/json"})
+        response = post.body
+        JSON.parse(response)
+      end
+
+  end
+end
