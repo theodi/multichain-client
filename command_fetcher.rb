ff1d27f48dafd0178750c67fa0fdb75bd476cad8
pikesley <github@orgraphone.org>
2016-01-06 23:09:06 +0000

    FFS

--- command_fetcher.rb
+++
@@ -1,7 +0,0 @@
-require 'multichain'
-
-c = Multichain::Client.new 'multichainrpc', '55nGoc9SbM6Usz3EcWfG8iXYNNqA1Y5MFGNt2d4VpT8V', 'chain.theodi.org'
-
-mess = c.help['result'].split "\n"
-
-puts mess 
